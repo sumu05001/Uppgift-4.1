826c9870ea8811e74cfb105b3cc714ddc68ed1d4
Tsnem Musa <tm222qf@student.lnu.se>
2023-03-06 19:41:32 +0100

    Add project files.

---
+++ Uppgift 4.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Uppgift_4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int talen = 10; talen <= 30; talen++)
+            {
+                Console.WriteLine(talen);
+            }
+            Console.WriteLine();
+
+            for (int talen = 200; talen >= 180; talen --)
+            {
+                Console.WriteLine(talen);
+            }
+            Console.WriteLine();
+
+            for (int talen = 1000; talen <= 1400; talen +=50)
+            {
+                Console.WriteLine(talen);
+            }
+
+        }  
+    }  
+}
+
